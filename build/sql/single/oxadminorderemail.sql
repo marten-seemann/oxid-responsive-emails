7ddde58e588d4096b917974ccdc871e30ff74d46
Marten Seemann <martenseemann@gmail.com>
2016-01-20 01:58:57 +0700

    commit changed SQL files
    changed by 13b5a5845199f8e9cb7d94e920c6c0719dc17b55

--- build/sql/single/oxadminorderemail.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `oxcontents` SET `OXCONTENT`='Folgende Artikel wurden soeben unter [{ $shop->oxshops__oxname->value }] bestellt:', `OXCONTENT_1`='The following products have been ordered in [{ $shop->oxshops__oxname->value }] right now:' WHERE `OXLOADID`='oxadminorderemail' AND `OXSHOPID`='oxbaseshop';
-
--- build/sql/single/oxuserorderemailend.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `oxcontents` SET `OXCONTENT`='<p>Fügen Sie hier Ihre Widerrufsbelehrung ein.</p>', `OXCONTENT_1`='<p>Right to Withdrawal can be inserted here.</p>' WHERE `OXLOADID`='oxuserorderemailend' AND `OXSHOPID`='oxbaseshop';
-
