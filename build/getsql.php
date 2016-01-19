13b5a5845199f8e9cb7d94e920c6c0719dc17b55
Marten Seemann <martenseemann@gmail.com>
2016-01-19 23:46:10 +0700

    avoid escaping problems and OXID bug during SQL import / export

--- build/getsql.php
+++ build/getsql.php
@@ -19,18 +19,24 @@ require(dirname(__FILE__)."/../../../bootstrap.php");
 $oDb = oxRegistry::get("oxDb");
 $db = $oDb->getDb();
 
+/*
+* by using the HEX() values of the OXCONTENT here, we avoid
+* 1. problems with escaping \r and \n
+*/
+
 foreach($ids as $id) {
-  $result = $db->getRow("SELECT OXCONTENT,OXCONTENT_1 FROM oxcontents WHERE OXLOADID='$id' AND OXSHOPID='oxbaseshop'");
+  $result = $db->getRow("SELECT HEX(OXCONTENT), HEX(OXCONTENT_1) FROM oxcontents WHERE OXLOADID='$id' AND OXSHOPID='oxbaseshop'");
 
   $content['de'] = $result[0];
   $content['en'] = $result[1];
 
-  $query = "UPDATE `oxcontents` SET `OXCONTENT`='".$oDb->escapeString($content['de'])."', `OXCONTENT_1`='".$oDb->escapeString($content['en'])."' WHERE `OXLOADID`='$id' AND `OXSHOPID`='oxbaseshop';\n\n";
+  $query = "UPDATE `oxcontents` SET
+    `OXCONTENT`=UNHEX('".$oDb->escapeString($content['de'])."'),
+    `OXCONTENT_1`=UNHEX('".$oDb->escapeString($content['en'])."')
+     WHERE `OXLOADID`='$id' AND `OXSHOPID`='oxbaseshop';\n\n";
 
   $filename_sql = "{$id}.sql";
   $filename_txt = "{$id}.txt";
   echo "Writing file: **".$save_dir.$filename_sql."**\n";
   file_put_contents($save_dir.$filename_sql, $query);
-  // file_put_contents($save_dir."de/".$filename_txt, $content['de']);
-  // file_put_contents($save_dir."en/".$filename_txt, $content['en']);
 }
--- build/getsql.sh
+++ build/getsql.sh
@@ -13,7 +13,5 @@ echo "Reading database records..."
 $CURL $GETSQL
 
 echo "Combining into a single file..."
-cat single/*.sql > combined.sql.tmp
-# cat combined.sql.tmp | sed 's/\\\\\\/\\/g' > combined.sql
-mv combined.sql.tmp combined.sql
-# cat combined.sql.tmp | sed 's/\\r/\\\\r/g' | sed 's/\\n/\\\\n/g' > combined.sql
+cat single/*.sql > combined.sql
+rm -r single
