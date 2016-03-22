2beaf2c55d4def885d278b22a6c43a90983ee124
Marten Seemann <martenseemann@gmail.com>
2016-03-22 14:12:06 +0700

    version bump

--- metadata.php
+++ metadata.php
@@ -12,7 +12,7 @@ $aModule = array(
       'en' => 'make OXID emails look great on all devices'
         ),
     'thumbnail'    => 'picture.png',
-    'version'      => '1.0',
+    'version'      => '1.1',
     'author'       => 'Marten Seemann',
     'url'          => 'https://shop.oxid-responsive.com',
     'blocks'       => array(
