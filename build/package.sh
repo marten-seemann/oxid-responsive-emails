a1fde08d6ec40f5ae8944c7baf00f71bbddba893
Marten Seemann <martenseemann@gmail.com>
2016-06-01 17:46:33 +0700

    fix module path in installation package

--- build/package.sh
+++ build/package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ZIP=/usr/bin/zip
 FILENAME="responsive_email.zip"
+MODULEPATH="modules/responsive_email"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #directory of the current script
 
@@ -9,11 +10,11 @@ cd $DIR
 rm -rf package
 mkdir package
 cd package
-mkdir copy_this changed_full
-cp -r $DIR/productive/* copy_this/
-mv copy_this/modified changed_full/
-mkdir changed_full/modified/html
-mv copy_this/sql .
+mkdir -p copy_this/$MODULEPATH changed_full/$MODULEPATH
+cp -r $DIR/productive/* copy_this/$MODULEPATH
+mv copy_this/$MODULEPATH/modified changed_full/$MODULEPATH
+mkdir changed_full/$MODULEPATH/modified/html
+mv copy_this/$MODULEPATH/sql .
 
 echo "Adding install instructions..."
 cp $DIR/install_instruction/README.txt .
